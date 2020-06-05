641bd996390db94843ba159533cca6077b4e9d62
henrybergstrom <henrybergstrom@protonmail.com>
2020-06-05 08:14:18 +0200

    installed fae

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+
+  namespace :admin do
+  end
+  # mount Fae below your admin namespec
+  mount Fae::Engine => '/admin'
+
+  root 'pages#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
