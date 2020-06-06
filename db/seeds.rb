d0a1c31c61ffafc20d93ec6425efb67d2da0e2ce
henrybergstrom <henrybergstrom@protonmail.com>
2020-06-06 19:33:51 +0200

    products page working

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: 'Balls', description: 'Tastes great!', quantity: 69, price: 4.20, manufacturer: "Deez", model: "Nutz")
+Product.create(name: 'Stuff', description: 'Tastes great!', quantity: 69, price: 4.20, manufacturer: "Deez", model: "Nutz")
+Product.create(name: 'You know', description: 'Tastes great!', quantity: 69, price: 4.20, manufacturer: "Deez", model: "Nutz")
+Product.create(name: 'This one', description: 'Tastes great!', quantity: 69, price: 4.20, manufacturer: "Deez", model: "Nutz")
+Product.create(name: 'Good', description: 'Tastes great!', quantity: 69, price: 4.20, manufacturer: "Deez", model: "Nutz")
