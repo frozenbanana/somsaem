91489746322bd5e67a066c7aeff3eff6d52f3d22
henrybergstrom <henrybergstrom@protonmail.com>
2020-06-07 11:55:56 +0200

    Added bootstrap auth forms

--- db/schema.rb
+++ db/schema.rb
@@ -50,18 +50,6 @@ ActiveRecord::Schema.define(version: 2020_06_06_225627) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "admins", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_admins_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
-  end
-
   create_table "article_categories", force: :cascade do |t|
     t.string "name"
     t.integer "position"
