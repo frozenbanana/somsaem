bcf9ebcd2dfddf4fbb798b02f74c534897962a66
henrybergstrom <henrybergstrom@protonmail.com>
2020-06-10 18:51:20 +0200

    Step one of three done

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   resources :search, only: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
+  get 'sales', to: 'sales#index'
 
   get '/admin', :to => 'pages#admin'
 end
