f6eb37f3ed0ed64e42c85bda408c80c91986d11f
Matthew Serre <matthew.serre@gmail.com>
2020-06-09 21:08:45 -0400

    Delete unnecessary controller methods and views for line_item

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,30 +1,7 @@
 class LineItemsController < ApplicationController
-
-  before_action :set_line_item, only: [:show, :edit, :update, :destroy]
+  before_action :set_line_item, only: [:destroy]
   before_action :set_cart, only: [:create]
 
-  # GET /line_items
-  # GET /line_items.json
-  def index
-    @line_items = LineItem.all
-  end
-
-  # GET /line_items/1
-  # GET /line_items/1.json
-  def show
-  end
-
-  # GET /line_items/new
-  def new
-    @line_item = LineItem.new
-  end
-
-  # GET /line_items/1/edit
-  def edit
-  end
-
-  # POST /line_items
-  # POST /line_items.json
   def create
     @product = Product.find(params[:product_id])
     @line_item = LineItem.new(product_id: @product.id, cart_id: session[:cart_id])
@@ -41,22 +18,6 @@ class LineItemsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /line_items/1
-  # PATCH/PUT /line_items/1.json
-  def update
-    respond_to do |format|
-      if @line_item.update(line_item_params)
-        format.html { redirect_to @line_item, notice: 'Line item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @line_item }
-      else
-        format.html { render :edit }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /line_items/1
-  # DELETE /line_items/1.json
   def destroy
     @line_item.destroy
     respond_to do |format|
@@ -66,13 +27,9 @@ class LineItemsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_line_item
       @line_item = LineItem.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
-    # def line_item_params
-    #   params.permit(:product_id, :cart_id)
-    # end
 end
