7df7f5d034e0730d01f5e283dc8cc51e63199170
Matthew Serre <matthew_serre+github@outlook.com>
2020-06-19 00:20:00 -0400

    Fix lineitemscontroller redirect path for certain methods

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -4,17 +4,50 @@ class LineItemsController < ApplicationController
 
   def create
     @product = Product.find(params[:product_id])
+<<<<<<< Updated upstream
     @line_item = LineItem.new(product_id: @product.id, cart_id: @cart.id)
     if @line_item.save
       redirect_to cart_path, notice: 'Item added successfully.'
     else
       redirect_to @product, notice: 'Could not add item to cart.  Please try again.'
+=======
+<<<<<<< Updated upstream
+    @line_item = LineItem.new(product_id: @product.id, cart_id: session[:cart_id])
+
+
+    respond_to do |format|
+      if @line_item.save
+        format.html { redirect_to @cart, notice: 'Item added successfully.' }
+        format.json { render :show, status: :created, location: @line_item }
+      else
+        format.html { redirect_to @product, notice: 'Could not add item to cart.  Please try again.'}
+        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+      end
+=======
+    @line_item = LineItem.new(product_id: @product.id, cart_id: @cart.id)
+    if @line_item.save
+      redirect_to mycart_path, notice: 'Item added successfully.'
+    else
+      redirect_to @product, notice: 'Could not add item to cart.  Please try again.'
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
     end
   end
 
   def destroy
     @line_item.destroy
+<<<<<<< Updated upstream
     redirect_to cart_path, notice: 'Item removed from cart successfully.'
+=======
+<<<<<<< Updated upstream
+    respond_to do |format|
+      format.html { redirect_to Cart.find(session[:cart_id]), notice: 'Item removed from cart successfully.' }
+      format.json { head :no_content }
+    end
+=======
+    redirect_to mycart_path, notice: 'Item removed from cart successfully.'
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
   end
 
   private
